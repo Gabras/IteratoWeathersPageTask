2d4690a17f9c93013ce92ebeec195c90ca9af592
Gabrielius Jurgelionis <gabrieliusjurg@gmail.com>
2021-10-31 18:28:32 +0200

    Ajax communication with server side created

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +18,18 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route("/weather", name="weather_getter")
+     * @param Request $request
+     */
+    public function getWeather(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        //Logic for 3rd party API
+
+        return new JsonResponse($data['city'], Response::HTTP_OK);
     }
 }
